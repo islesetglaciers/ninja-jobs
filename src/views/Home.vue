1153df7ee01eae7bbd04e1a1b0152336dbede276
sidd sirius <sidd.benoit@gmail.com>
2021-02-17 15:00:54 -0500

    og setup

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,16 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Homepage</h1>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Amet maiores, sapiente consectetur aliquid molestiae laboriosam optio mollitia, quas nulla cum, facere nihil debitis ducimus accusantium ipsum! Voluptas excepturi molestiae animi accusamus facilis ullam voluptates, perspiciatis quasi fugiat repellendus earum molestias reprehenderit deserunt blanditiis totam? Asperiores reiciendis consequuntur officiis iste sed veritatis. Omnis deleniti natus vitae molestiae nihil! Assumenda quisquam velit, asperiores culpa libero cupiditate. Necessitatibus voluptas temporibus, laboriosam ut corporis dolor. Nam quisquam asperiores ipsum nostrum aliquam quod, explicabo temporibus provident atque hic doloribus cumque alias perspiciatis dolore reiciendis ab quae, rem impedit accusamus consequatur, aperiam maiores saepe tempore minima.</p>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Amet maiores, sapiente consectetur aliquid molestiae laboriosam optio mollitia, quas nulla cum, facere nihil debitis ducimus accusantium ipsum! Voluptas excepturi molestiae animi accusamus facilis ullam voluptates, perspiciatis quasi fugiat repellendus earum molestias reprehenderit deserunt blanditiis totam? Asperiores reiciendis consequuntur officiis iste sed veritatis. Omnis deleniti natus vitae molestiae nihil! Assumenda quisquam velit, asperiores culpa libero cupiditate. Necessitatibus voluptas temporibus, laboriosam ut corporis dolor. Nam quisquam asperiores ipsum nostrum aliquam quod, explicabo temporibus provident atque hic doloribus cumque alias perspiciatis dolore reiciendis ab quae, rem impedit accusamus consequatur, aperiam maiores saepe tempore minima.</p>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
-  }
+  components: {  }
 }
 </script>
